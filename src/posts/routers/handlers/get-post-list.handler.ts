37153ef45ea0501777d19b499bf7cda245cde73a
Ivan S <xsmirx@gmail.com>
2025-06-01 18:16:39 +0400

    feat: enhance getPostListHandler to include blog names and improve input validation middleware for post fields

--- src/posts/routers/handlers/get-post-list.handler.ts
+++ src/posts/routers/handlers/get-post-list.handler.ts
@@ -1,8 +1,15 @@
 import { Request, Response } from 'express';
 import { postsRepository } from '../../repositories/posts.repository';
 import { HttpStatus } from '../../../core/types/http-statuses';
+import { blogsRepository } from '../../../blogs/repositories/blogs.repository';
 
 export const getPostListHandler = (req: Request, res: Response) => {
   const posts = postsRepository.findAll();
-  res.status(HttpStatus.Ok).send(posts);
+  res.status(HttpStatus.Ok).send(
+    posts.map((post) => ({
+      ...post,
+      blogName:
+        blogsRepository.findById(post.blogId)?.name || 'Unknown Blog Name',
+    })),
+  );
 };
--- src/posts/validation/post.input-dto.validation-middleware.ts
+++ src/posts/validation/post.input-dto.validation-middleware.ts
@@ -4,24 +4,28 @@ import { blogsRepository } from '../../blogs/repositories/blogs.repository';
 const titleValidation = body('title')
   .isString()
   .withMessage('Title must be a string')
+  .trim()
   .isLength({ min: 1, max: 20 })
   .withMessage('Title must be between 1 and 20 characters long');
 
 const shortDescriptionValidation = body('shortDescription')
   .isString()
   .withMessage('Short description must be a string')
+  .trim()
   .isLength({ min: 1, max: 100 })
   .withMessage('Short description must be between 1 and 100 characters long');
 
 const contentValidation = body('content')
   .isString()
   .withMessage('Content must be a string')
+  .trim()
   .isLength({ min: 1, max: 1000 })
   .withMessage('Content must be between 1 and 1000 characters long');
 
 const blogIdValidation = body('blogId')
   .isString()
   .withMessage('Blog ID must be a string')
+  .trim()
   .custom(async (blogId: string) => {
     const blog = blogsRepository.findById(blogId);
     if (!blog) {
